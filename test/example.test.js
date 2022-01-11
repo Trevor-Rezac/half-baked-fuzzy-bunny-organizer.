2a94fa9c09d6cc5696c83543a7e5a652771bb564
TrevorRezac <trev.rezac@gmail.com>
2022-01-10 16:25:25 -0800

    TDD for renderBunny

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,23 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { renderBunny } from '../render-utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('renderBunny(bunny) takes in a bunny object and returns a dom element - a p tag with class bunny and a name as text content', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const bunny = {
+        name: 'Hopper',
+        family_id: 1,
+    };
+
+    const expected = '<p class="bunny">Hopper</p>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderBunny(bunny);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected, 'renderBunny(bunny) takes in a bunny object and returns a p tag with name Hopper');
 });
